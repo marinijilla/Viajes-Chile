301b9c33956364a443110b9caa446b53dd733232
Marina Parra <marina.parra.a@gmail.com>
2022-04-22 19:28:10 -0400

    Commit 5
    Se otorga padding a títulos

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,14 @@ $(function(){
         });
       }
     });
-  
+
+    $(document).ready(function(){
+      $('[data-toggle="tooltip"]').tooltip();   
+    });
+
+    
+
+    
     $(function () {
       $('[data-toggle="tooltip"]').tooltip()
     })
